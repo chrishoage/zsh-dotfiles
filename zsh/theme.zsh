7c50872791c0ea1dac6ea5fee1d08ed0485367a9
Chris Hoage <iam@chrishoage.com>
2016-01-12 22:01:01 -0800

    no more changing prompt char

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,11 +1,6 @@
-function prompt_char {
-    git branch >/dev/null 2>/dev/null && echo '±' && return
-    echo '❯'
-}
-
 PROMPT='
 %{$fg[magenta]%}%n%{$reset_color%} at %{$fg[yellow]%}%m%{$reset_color%} in %{$fg_bold[green]%}${PWD/#$HOME/~}%{$reset_color%}$(git_prompt_info)
-%(?,%{%F{white}%},%{%F{red}%})$(prompt_char)%{$reset_color%} '
+%(?,%{%F{white}%},%{%F{red}%})❯%{$reset_color%} '
 
 ZSH_THEME_GIT_PROMPT_PREFIX=" on %{$fg[magenta]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
